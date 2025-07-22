01e2f380c94aef2ef3e1553e085d0138c1908095
purva jage <purvajage44@gmail.com>
2025-07-22 10:15:49 +0530

    commit

--- logout.php
+++ logout.php
@@ -7,10 +7,7 @@
 	if (isset($_COOKIE[session_name()])) {
 		setcookie(session_name(), '', time()-86400, '/');
 	}
-
 	session_destroy();
-
-	// redirecting the user to the login page
 	header('Location: login.php?action=logout');
 
  ?>
