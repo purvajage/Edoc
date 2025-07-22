cab21233593c2256fd8a3b86e51af18cf630d659
purva jage <purvajage44@gmail.com>
2025-07-22 10:16:08 +0530

    commit

--- connection.php
+++ connection.php
@@ -3,9 +3,7 @@ $servername = "localhost";
 $username = "root";  
 $password = ""; 
 $dbname = "edoc"; 
-
 $database = new mysqli($servername, $username, $password, $dbname);
-
 if ($database->connect_error) {
     die("Échec de la connexion : " . $database->connect_error);
 }
